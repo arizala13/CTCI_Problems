e46cc86f4fd8f8f0f904a913d4504ebbad4d1d49
Andres Arizala <arizala13@gmail.com>
2020-08-25 08:08:42 -0500

    Work on Chapter 2 Problems

--- 2.1.py
+++ 2.1.py
@@ -5,3 +5,16 @@
 # Time complexity:
 # Space complexity:
 
+def remove_duplicates(passed_list):
+    new_list = []
+    for num in passed_list:
+        if num in new_list:
+            continue
+        else:
+            new_list.append(num)
+
+    print(new_list)
+
+
+remove_duplicates([10, 13, 18, 8, 8, 4, 10, 14])
+# what does a test case for a linked list look like?
