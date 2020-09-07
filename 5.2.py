9a216bd464c7f38a216a618ef8cdb827b973d349
Andres Arizala <arizala13@gmail.com>
2020-09-07 17:31:17 -0500

    Adding 2 questions for chapter 5

---
+++ 5.2.py
@@ -0,0 +1,8 @@
+# This is question 5.2 of CTCI
+#  ------ Notes to myself ----------
+# Binary to String: Given a real number between O and 1 (e.g., 0.72)
+# that is passed in as a double, print the binary
+# representation. If the number cannot be represented
+# accurately in binary with at most 32 characters, print "ERROR:'
+# Time complexity:
+# Space complexity:
