a9582946a5b9720a235f40c4be7daa40f59622f2
Andres Arizala <arizala13@gmail.com>
2020-09-08 10:45:39 -0500

    adding 2 questions for chapter 10

---
+++ 10.2.py
@@ -0,0 +1,8 @@
+# This is question 10.2 of CTCI
+#  ------ Notes to myself ----------
+# Group Anagrams: Write a method to sort an array
+# of strings so that all the anagrams are next to
+# each other.
+# the bottom right.
+# Time complexity:
+# Space complexity:
