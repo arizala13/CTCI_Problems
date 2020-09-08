cf430c4bfe1c9ab62778270c96db9d5c9c18b73a
Andres Arizala <arizala13@gmail.com>
2020-09-08 09:30:31 -0500

    Adding 2 questions form chapter 8

---
+++ 8.1.py
@@ -0,0 +1,8 @@
+# This is question 8.1 of CTCI
+#  ------ Notes to myself ----------
+# Triple Step: A child is running up a staircase with n steps
+# and can hop either 1 step, 2 steps, or 3 steps at a time.
+# Implement a method to count how many possible ways the child
+# can run up the stairs.
+# Time complexity:
+# Space complexity:
